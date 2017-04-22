f610e5e55188aa4d8f68b025dc9388ec9f04bc2f
Артем Политов <politovartem@gmail.com>
2017-04-22 20:07:01 +0300

    Added logger interface for providers

---
+++ IdentityServer/IdentityServer.AuthorizationProvider.ApsNetIdenityEf/Provider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IdentityServer.AuthorizationProvider.ApsNetIdenityEf
+{
+    public class Provider : IRegistrationProvider
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(IDictionary<string, string> parameters, ILogger log)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> Authorize(string user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> Register(string user, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IdentityServer/IdentityServer.AuthorizationProvider/ILogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IdentityServer.AuthorizationProvider
+{
+    public interface ILogger
+    {
+        void Trace(string message);
+        void Trace(string message, Exception exception);
+
+        void Debug(string message);
+        void Debug(string message, Exception exception);
+
+        void Info(string message);
+        void Info(string message, Exception exception);
+
+        void Warning(string message);
+        void Warning(string message, Exception exception);
+
+        void Error(string message);
+        void Error(string message, Exception exception);
+
+        void Fatal(string message);
+        void Fatal(string message, Exception exception);
+    }
+}
