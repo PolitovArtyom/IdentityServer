d49a132fe813bd97d694819b8e4bad2bc8c99a4e
Politov Artyom <politovartem@gmail.com>
2017-04-23 13:30:42 +0300

    add IdentityServer.Core project

---
+++ IdentityServer/IdentityServer/App_Start/IocConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Optimization;
+using Autofac;
+
+namespace IdentityServer
+{
+    public class IocConfug
+    {
+
+        public void Setup()
+        {
+            var builder = new ContainerBuilder();
+        }
+        
+    }
+}
